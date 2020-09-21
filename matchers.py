82d0249f5ccffa4a76bfb5667e026d40db48d4ad
galoz <gal.oz@powtoon.com>
2020-09-21 13:51:19 +0300

    fill objects with logic

--- matchers.py
+++ matchers.py
@@ -1,4 +1,6 @@
+import os
 from abc import ABC, abstractmethod
+import re
 
 
 class BaseMatcherMeta(ABC):
@@ -9,7 +11,7 @@ class BaseMatcherMeta(ABC):
     def print_verbose_path(self):
         pass
 
-    def print_with_carret(self):
+    def print_with_caret(self):
         pass
 
     def print_normal(self):
@@ -17,13 +19,42 @@ class BaseMatcherMeta(ABC):
 
     @property
     @abstractmethod
-    def line_dict(self):
+    def line_dict(self) -> dict:
         pass
 
 
-class StringMatcher:
-    pass
+class StringMatcher(BaseMatcherMeta):
+
+    def __init__(self, string: str, regex):
+        self.string = string
+        self.regex = regex
+
+    @property
+    def line_dict(self):
+        l_dict = dict()
+        for idx, line in enumerate(self.string.split('\n')):
+            result = re.finditer(self.regex, line)
+            if len(list(result)):
+                l_dict[idx] = result
+        return l_dict
 
 
-class FileMatcher:
-    pass
+class FileMatcher(BaseMatcherMeta):
+
+    def __init__(self, file: str, regex: str):
+        if os.path.isfile(file):
+            self.file = file
+        else:
+            raise FileNotFoundError(f"Cannot find file {file}")
+        self.regex = regex
+
+    @property
+    def line_dict(self):
+        l_dict = dict()
+        with open(self.file) as f:
+            for idx, line in enumerate(f):
+                result = re.finditer(self.regex, line)
+                if len(list(result)):
+                    l_dict[idx] = result
+        return l_dict
+
---
+++ regex_matcher.py
@@ -0,0 +1,33 @@
+import sys
+
+from matchers import StringMatcher, FileMatcher
+import argparse
+
+GREEN_COLOR = '\033[92m'
+END_STYLE = '\x1b[0m'
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-r', '--regex', help="regex expression for searching")
+parser.add_argument('-f', '--files', help="list of files to search in")
+
+# mutually exclusive:
+parser.add_argument('-u', '--underline', help="print matches with '^' underneath them")
+parser.add_argument('-c', '--color', help="print colored matches")
+parser.add_argument('-m', '--machine', help="print matches with 'print the output in the format:"
+                                            "\n'file_name:line_number:start_position:matched_text'")
+
+args = parser.parse_args()
+
+
+def main():
+    if not args.regex:
+        print("please provide a regex pattern!")
+
+    if args.files:
+        matchers = [FileMatcher(file) for file in args.files.split()]
+    else:
+        matchers = StringMatcher(sys.argv[-1])
+
+
